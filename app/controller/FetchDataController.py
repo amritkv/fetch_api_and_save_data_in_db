85c357658e4acae5c8c046b43ca75947af7ad2b8
amritkv7 <er.akverma8@gmail.com>
2022-06-24 00:38:57 +0530

    Minor code fixing

--- app/controller/FetchDataController.py
+++ app/controller/FetchDataController.py
@@ -5,16 +5,17 @@ import requests
 from flask import Flask
 from dotenv import load_dotenv, find_dotenv
 
-app = Flask("FetchData")
+app = Flask("FetchAndData")
 load_dotenv(find_dotenv('./config/.env'))
 app_id = os.getenv('APP_ID')
 URL = "https://dummyapi.io/data/v1/user"
 
-@app.route('/fetchData', methods=['GET'])
+@app.route('/fetchAndSaveData', methods=['GET'])
 def fetchData():
     request_header = {"Content-Type": "application/json", "app-id" : app_id}
     response = requests.get(url=URL, headers=request_header)    
     return response.content
 
+@app.route('')
 
 
--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-from flask_cors import CORS, cross_origin
 from app.controller.FetchDataController import app
-
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-
 		
 if __name__ == '__main__':
    app.run(host='127.0.0.1', port='5000', debug = True)
