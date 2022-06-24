0f9c147ca4eaae5e6b8a3385e651714d28b69ccc
amritkv7 <er.akverma8@gmail.com>
2022-06-24 23:54:15 +0530

    updated code for 2nd api call to fetch users post data

--- app/controller/FetchDataController.py
+++ app/controller/FetchDataController.py
@@ -12,14 +12,18 @@ class fetchAndSave:
     def __init__(self):
         pass
 
-    @app.route('/fetchAndSaveData', methods=['GET'])
+    @app.route('/fetchAndSaveData', methods = ['GET'])
     def fetchData():
 
-        fetch_data_status = ServiceLayer.fetchAndSaveService().fetchUersData()
+        fetch_data_status = ServiceLayer.fetchAndSaveService().fetchUersDataService()
         if fetch_data_status == 200:
-            return json.dumps({"Message" : "Successfully data saved into database....!"})
+            return json.dumps({"Message" : "Successfully saved users data into database....!"})
 
         else:
             return json.dumps({"Error" : fetch_data_status})
 
+    @app.route('/fetchUsersPostData', methods = ['GET'])
+    def fetchUsersPostData():
 
+        fetch_posts_data_status = ServiceLayer.fetchAndSaveService().fetchUsersPostDataService()
+        
--- app/dataAccessLayer/FetchAndSaveDataLayer.py
+++ app/dataAccessLayer/FetchAndSaveDataLayer.py
@@ -27,22 +27,37 @@ class FetchAndSaveDataLayer:
     def fetchUsersData(self):
 
         db=self.dbConnection.getDbConnection()
-        # if db.is_connected():
-        #     print('Database connected for saving users data....!')
         db_name = self.DB_NAME
-        print(db_name)
         collection_name = self.USERS_COLLECTION_NAME
         try:
             request_header = {"Content-Type": "application/json", "app-id" : self.APP_ID}
             response = requests.get(url=self.USERS_DATA_URL, headers=request_header)
-            # print(response.json())
             
-            for data in response.json()['data']:
-                db[self.DB_NAME][self.USERS_COLLECTION_NAME].insert_one(data)
-            return response.status_code
+            try:
+                for data in response.json()['data']:
+                    db[self.DB_NAME][self.USERS_COLLECTION_NAME].insert_one(data)
+                return response.status_code
+            
+            except Exception as excep:
+                print("Could not save users data into database :", error)
+                print(error.__class__, "occurred.")
+                logging.exception(str(error))   
         
         except Exception as error:
-            
             print("Error occurred :", error)
             print(error.__class__, "occurred.")
             logging.exception(str(error))
+
+    def fetchUsersPostData(self):
+        
+        db=self.dbConnection.getDbConnection()
+        db_name = self.DB_NAME
+        collection_name = self.USERS_COLLECTION_NAME
+        try:
+            users_data = db[self.DB_NAME][self.USERS_COLLECTION_NAME].find()
+            print(users_data)
+            # for each_user_data in 
+        except Exception as excep:
+            print("Error occurred :", excep)
+            print(excep.__class__, "occurred.")
+            logging.exception(str(excep))
